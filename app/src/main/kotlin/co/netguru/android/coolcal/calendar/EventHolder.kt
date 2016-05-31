240ba0710a276e4c435274d10fb74659136a52f0
Przemysław Dąbrowski <przemyslaw.dabrowski@netguru.co>
2016-05-31 13:40:51 +0200

    [JIR-71] Small design corrections on the calendar screen.

--- app/src/main/kotlin/co/netguru/android/coolcal/calendar/EventHolder.kt
+++ app/src/main/kotlin/co/netguru/android/coolcal/calendar/EventHolder.kt
@@ -3,6 +3,7 @@ package co.netguru.android.coolcal.calendar
 import android.content.res.Resources
 import android.view.View
 import android.widget.ImageView
+import android.widget.Space
 import android.widget.TextView
 import co.netguru.android.coolcal.R
 import co.netguru.android.coolcal.app.App
@@ -48,23 +49,32 @@ class EventHolder(itemView: View) : ViewHolder(itemView) {
     val weatherIconImageView: ImageView by lazy {
         itemView.findViewById(R.id.event_weather_icon) as ImageView
     }
+    val eventTimeSpace: Space by lazy {
+        itemView.findViewById(R.id.event_time_space) as Space
+    }
+    val eventTitleSpace: Space by lazy {
+        itemView.findViewById(R.id.event_title_space) as Space
+    }
 
     fun bind(obj: Event, forecast: Forecast?) {
         titleTextView.text = obj.title ?: ""
         timeTextView.text = timeFormatter.formatTimeOfDay(obj.begin, obj.isAllDay)
         durationTextView.text = timeFormatter.formatPeriod(obj.begin, obj.end, obj.isAllDay)
-        if (forecast == null || obj.isAllDay) {
-            temperatureTextView.visibility = View.GONE
-            weatherIconImageView.visibility = View.GONE
+        if (obj.isAllDay) {
+            setEventViewsVisibility(View.GONE)
         } else {
-            val temperature = forecast.main?.temperature
-            val icon = forecast.weatherList[0].icon
-            temperatureTextView.visibility = View.VISIBLE
-            weatherIconImageView.visibility = View.VISIBLE
-            temperatureTextView.text = weatherDataFormatter.formatTemperature(temperature)
-            weatherIconImageView.setImageResource(weatherDecoder.getIconRes(icon))
-            messageTextView.text = weatherDescription.getDescription(icon, temperature,
-                    forecast.main?.pressure)
+            if (forecast == null) {
+                temperatureTextView.visibility = View.GONE
+                weatherIconImageView.visibility = View.GONE
+            } else {
+                val temperature = forecast.main?.temperature
+                val icon = forecast.weatherList[0].icon
+                setEventViewsVisibility(View.VISIBLE)
+                temperatureTextView.text = weatherDataFormatter.formatTemperature(temperature)
+                weatherIconImageView.setImageResource(weatherDecoder.getIconRes(icon))
+                messageTextView.text = weatherDescription.getDescription(icon, temperature,
+                        forecast.main?.pressure)
+            }
         }
         if (isCurrentEvent(obj.begin, obj.end) && !obj.isAllDay) {
             time_Indicator.visibility = View.VISIBLE
@@ -77,5 +87,14 @@ class EventHolder(itemView: View) : ViewHolder(itemView) {
         var currentTime = DateTime(System.currentTimeMillis()).toLocalDateTime()
         return DateTime(begin).toLocalDateTime().isBefore(currentTime)
                 && DateTime(end).toLocalDateTime().isAfter(currentTime)
+    }
+
+    private fun setEventViewsVisibility(visibility: Int) {
+        temperatureTextView.visibility = visibility
+        weatherIconImageView.visibility = visibility
+        timeTextView.visibility = visibility
+        messageTextView.visibility = visibility
+        eventTimeSpace.visibility = visibility
+        eventTitleSpace.visibility = visibility
     }
 }
